57f3b8329b1672f9d981cc06a01de478dc67308f
Danielprata07 <danielpcosme@gmail.com>
2024-09-26 12:20:48 -0300

    conexao criada

---
+++ srv/conexao.py
@@ -0,0 +1,15 @@
+import mysql 
+import mysql.connector
+
+conn = mysql.connector.connect(
+    username = 'prata',
+    host = 'localhost',
+    password = 'projeto020907',
+    db = 'projeto_crud'
+)
+
+''' 
+if conn.is_connected():
+    print('conectado com o banco')
+else:
+    print('nao conectado com o banco')'''
